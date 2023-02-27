480bf4c05d4118d4cd080e886d4efbafa34ff185
younggam <nainjoo2001@naver.com>
2023-02-28 01:38:56 +0900

    12205

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ fn main() {
     let mut lines = stdin().lines();
     let t: usize = lines.next().unwrap().unwrap().parse().unwrap();
 
+    let mut dp = [[0u64; 102]; 102];
+    dp[1][0] = 1;
+    for i in 1..=101 {
+        for j in i..=101 {
+            dp[i][j] = dp[i][j - 1].saturating_add(dp[i - 1][j]);
+        }
+    }
+
     for i in 1..=t {
         let input = lines.next().unwrap().unwrap();
         let mut vars = input.split_whitespace();
         let n: usize = vars.next().unwrap().parse().unwrap();
         let mut k: u64 = vars.next().unwrap().parse().unwrap();
 
-        let mut dp = vec![vec![0; n + 2]; n + 2];
-        dp[1][0] = 1;
-        for i in 1..=n + 1 {
-            for j in i..=n + 1 {
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-            }
-        }
         let mut x = n;
         let mut y = n + 1;
         if k > dp[x][y] {
