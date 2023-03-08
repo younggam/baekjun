0b60fb81491e14d5ad01418a96afba003bc432ed
younggam <nainjoo2001@naver.com>
2023-03-08 17:45:16 +0900

    yay 27654

--- examples/27654.rs
+++ examples/27654.rs
@@ -1,138 +1,17 @@
-// 시험(Fail)
-use std::{cmp::Ordering, io::stdin};
+// 시험
+use std::io::stdin;
 
-#[derive(Eq, PartialEq, Ord, PartialOrd)]
+const MAX_ERROR: f64 = 0.000001;
+
+#[derive(Copy, Clone)]
 struct Score {
     p: u32,
     q: u32,
 }
 
-#[derive(Eq, PartialEq)]
-struct Rate<'a> {
-    i: usize,
-    score: &'a Score,
-}
-
-impl<'a> PartialOrd for Rate<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let rate_self = self.score.p as f64 / self.score.q as f64;
-        let rate_other = other.score.p as f64 / other.score.q as f64;
-        match rate_self.partial_cmp(&rate_other) {
-            Some(Ordering::Equal) => {
-                match self
-                    .score
-                    .q
-                    .partial_cmp(&other.score.q)
-                    .map(Ordering::reverse)
-                {
-                    Some(Ordering::Equal) => self.i.partial_cmp(&other.i),
-                    o @ _ => o,
-                }
-            }
-            o @ _ => o,
-        }
-    }
-}
-
-impl<'a> Ord for Rate<'a> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-#[derive(Eq, PartialEq)]
-struct Diff<'a> {
-    i: usize,
-    score: &'a Score,
-}
-
-impl<'a> PartialOrd for Diff<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self.score.q - self.score.p)
-            .partial_cmp(&(other.score.q - other.score.p))
-            .map(Ordering::reverse)
-        {
-            Some(Ordering::Equal) => match self
-                .score
-                .q
-                .partial_cmp(&other.score.q)
-                .map(Ordering::reverse)
-            {
-                Some(Ordering::Equal) => self.i.partial_cmp(&other.i),
-                o @ _ => o,
-            },
-            o @ _ => o,
-        }
-    }
-}
-
-impl<'a> Ord for Diff<'a> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-#[derive(Eq, PartialEq)]
-struct Add<'a> {
-    i: usize,
-    score: &'a Score,
-}
-
-impl<'a> PartialOrd for Add<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self.score.q + self.score.p)
-            .partial_cmp(&(other.score.q + other.score.p))
-            .map(Ordering::reverse)
-        {
-            Some(Ordering::Equal) => match self
-                .score
-                .q
-                .partial_cmp(&other.score.q)
-                .map(Ordering::reverse)
-            {
-                Some(Ordering::Equal) => self.i.partial_cmp(&other.i),
-                o @ _ => o,
-            },
-            o @ _ => o,
-        }
-    }
-}
-
-impl<'a> Ord for Add<'a> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-#[derive(Eq, PartialEq)]
-struct Mul<'a> {
-    i: usize,
-    score: &'a Score,
-}
-
-impl<'a> PartialOrd for Mul<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self.score.q as u64 * self.score.p as u64)
-            .partial_cmp(&(other.score.q as u64 * other.score.p as u64))
-            .map(Ordering::reverse)
-        {
-            Some(Ordering::Equal) => match self
-                .score
-                .q
-                .partial_cmp(&other.score.q)
-                .map(Ordering::reverse)
-            {
-                Some(Ordering::Equal) => self.i.partial_cmp(&other.i),
-                o @ _ => o,
-            },
-            o @ _ => o,
-        }
-    }
-}
-
-impl<'a> Ord for Mul<'a> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
+impl Score {
+    fn as_line_equation(&self, x: f64) -> f64 {
+        -(self.q as f64) * x + self.p as f64
     }
 }
 
@@ -140,68 +19,45 @@ fn main() {
     let mut lines = stdin().lines();
     let input = lines.next().unwrap().unwrap();
     let mut vars = input.split_whitespace();
-    let n = vars.next().unwrap().parse().unwrap();
+    let n: usize = vars.next().unwrap().parse().unwrap();
     let k: u32 = vars.next().unwrap().parse().unwrap();
 
-    let mut scores = Vec::with_capacity(n);
-    for _ in 0..n {
+    let mut scores = [Score { p: 0, q: 0 }; 100000];
+    for i in 0..n {
         let input = lines.next().unwrap().unwrap();
         let mut exam = input.split_whitespace();
-        let score = Score {
-            p: exam.next().unwrap().parse().unwrap(),
-            q: exam.next().unwrap().parse().unwrap(),
-        };
-        scores.push(score);
+        scores[i].p = exam.next().unwrap().parse().unwrap();
+        scores[i].q = exam.next().unwrap().parse().unwrap();
     }
+    let scores_slice = &mut scores[0..n];
 
-    let mut rates = Vec::with_capacity(n);
-    let mut diffs = Vec::with_capacity(n);
-    let mut adds = Vec::with_capacity(n);
-    let mut muls = Vec::with_capacity(n);
-    for (i, score) in scores.iter().enumerate() {
-        rates.push(Rate { i, score });
-        diffs.push(Diff { i, score });
-        adds.push(Add { i, score });
-        muls.push(Mul { i, score });
-    }
-    rates.sort();
-    diffs.sort();
-    adds.sort();
-    muls.sort();
-
-    let mut p_accum = 0u64;
-    let mut q_accum = 0u64;
-    for _ in 0..k {
-        let rate = rates.last().unwrap();
-        let diff = diffs.last().unwrap();
-        let add = adds.last().unwrap();
-        let mul = muls.last().unwrap();
-        let if_rate =
-            (p_accum as f64 + rate.score.p as f64) / (q_accum as f64 + rate.score.q as f64);
-        let if_diff =
-            (p_accum as f64 + diff.score.p as f64) / (q_accum as f64 + diff.score.q as f64);
-        let if_add = (p_accum as f64 + add.score.p as f64) / (q_accum as f64 + add.score.q as f64);
-        let if_mul = (p_accum as f64 + mul.score.p as f64) / (q_accum as f64 + mul.score.q as f64);
-
-        let (i, score) = if if_rate >= if_diff && if_rate >= if_add && if_rate >= if_mul {
-            println!("!rate {} {}", rate.score.p, rate.score.q);
-            (rate.i, rate.score)
-        } else if if_diff > if_rate && if_diff > if_add && if_diff > if_mul {
-            println!("!diff {} {}", diff.score.p, diff.score.q);
-            (diff.i, diff.score)
-        } else if if_add > if_rate && if_add > if_diff && if_add > if_mul {
-            println!("!add_ {} {}", add.score.p, add.score.q);
-            (add.i, add.score)
+    let mut min = 0.;
+    let mut max = 1.;
+    loop {
+        scores_slice.sort_by(|a: &Score, b: &Score| {
+            let pivot = (min + max) * 0.5;
+            a.as_line_equation(pivot)
+                .partial_cmp(&b.as_line_equation(pivot))
+                .unwrap()
+        });
+        let mut scores_iter = scores_slice.iter();
+        let mut p_accum = 0u64;
+        let mut q_accum = 0u64;
+        for _ in 0..k {
+            let score = scores_iter.next_back().unwrap();
+            p_accum += score.p as u64;
+            q_accum += score.q as u64;
+        }
+        let average = p_accum as f64 / q_accum as f64;
+        let pivot = (min + max) * 0.5;
+        let diff = (average - pivot).abs();
+        if diff <= MAX_ERROR || diff / pivot <= MAX_ERROR {
+            println!("{}", average);
+            break;
+        } else if average >= pivot {
+            min = pivot;
         } else {
-            println!("!mul_ {} {}", mul.score.p, mul.score.q);
-            (mul.i, mul.score)
-        };
-        p_accum += score.p as u64;
-        q_accum += score.q as u64;
-        rates.remove(rates.binary_search(&Rate { i, score }).unwrap());
-        diffs.remove(diffs.binary_search(&Diff { i, score }).unwrap());
-        adds.remove(adds.binary_search(&Add { i, score }).unwrap());
-        muls.remove(muls.binary_search(&Mul { i, score }).unwrap());
+            max = pivot;
+        }
     }
-    println!("{}", p_accum as f64 / q_accum as f64);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::io::stdin;
-use std::time::Instant;
 
-#[derive(Eq, PartialEq, Ord, PartialOrd)]
+const MAX_ERROR: f64 = 0.000001;
+
+#[derive(Copy, Clone)]
 struct Score {
     p: u32,
     q: u32,
+}
+
+impl Score {
+    fn as_line_equation(&self, x: f64) -> f64 {
+        -(self.q as f64) * x + self.p as f64
+    }
 }
 
 fn main() {
@@ -14,57 +21,42 @@ fn main() {
     let n: usize = vars.next().unwrap().parse().unwrap();
     let k: u32 = vars.next().unwrap().parse().unwrap();
 
-    let mut first = 0;
-    let mut scores = Vec::with_capacity(n);
+    let mut scores = [Score { p: 0, q: 0 }; 100000];
     for i in 0..n {
         let input = lines.next().unwrap().unwrap();
         let mut exam = input.split_whitespace();
-        let score = Score {
-            p: exam.next().unwrap().parse().unwrap(),
-            q: exam.next().unwrap().parse().unwrap(),
-        };
-        scores.push(score);
-        let score = &scores[i];
-        let rate = score.p as f64 / score.q as f64;
-        let first_score = &scores[first];
-        let first_rate = first_score.p as f64 / first_score.q as f64;
-        if first_rate == rate {
-            if score.q > first_score.q {
-                first = i;
-            }
-        } else if first_rate < rate {
-            first = i;
+        scores[i].p = exam.next().unwrap().parse().unwrap();
+        scores[i].q = exam.next().unwrap().parse().unwrap();
+    }
+    let scores_slice = &mut scores[0..n];
+
+    let mut min = 0.;
+    let mut max = 1.;
+    loop {
+        scores_slice.sort_by(|a: &Score, b: &Score| {
+            let pivot = (min + max) * 0.5;
+            a.as_line_equation(pivot)
+                .partial_cmp(&b.as_line_equation(pivot))
+                .unwrap()
+        });
+        let mut scores_iter = scores_slice.iter();
+        let mut p_accum = 0u64;
+        let mut q_accum = 0u64;
+        for _ in 0..k {
+            let score = scores_iter.next_back().unwrap();
+            p_accum += score.p as u64;
+            q_accum += score.q as u64;
+        }
+        let average = p_accum as f64 / q_accum as f64;
+        let pivot = (min + max) * 0.5;
+        let diff = (average - pivot).abs();
+        if diff <= MAX_ERROR || diff / pivot <= MAX_ERROR {
+            println!("{}", average);
+            break;
+        } else if average >= pivot {
+            min = pivot;
+        } else {
+            max = pivot;
         }
     }
-    let time = Instant::now();
-
-    let first = scores.swap_remove(first);
-    println!("! {} {}", first.p, first.q);
-    let mut p_accum = first.p as u64;
-    let mut q_accum = first.q as u64;
-    for _ in 1..k {
-        let mut i = 0;
-        for j in 1..scores.len() {
-            let cur = &scores[i];
-            let score = &scores[j];
-            let rate_with_score =
-                (p_accum as f64 + score.p as f64) / (q_accum as f64 + score.q as f64);
-            let rate_with_cur = (p_accum as f64 + cur.p as f64) / (q_accum as f64 + cur.q as f64);
-            if rate_with_cur < rate_with_score {
-                i = j;
-            } else if rate_with_cur == rate_with_score && cur.q > score.q {
-                i = j;
-            }
-        }
-        let score = scores.swap_remove(i);
-        println!("! {} {}", score.p, score.q);
-
-        p_accum += score.p as u64;
-        q_accum += score.q as u64;
-    }
-    println!(
-        "{} {}",
-        p_accum as f64 / q_accum as f64,
-        time.elapsed().as_secs_f64()
-    );
 }
